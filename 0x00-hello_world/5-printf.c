f4cdfda325bc92746e1281e76dd332a1874acf06
joshua obaji <joshuaemeka2022@gmail.com>
2022-09-12 01:03:55 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints the word with proper grammar,
+ * main - Entry point
+ *
  * but the outcome is a piece of art,
- * Return - zero if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, else otherwise
  */
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
