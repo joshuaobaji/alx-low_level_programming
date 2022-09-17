b3f456c20ecf946e95b5fa7445a1533d1731330f
joshua obaji <joshuaemeka2022@gmail.com>
2022-09-16 20:59:57 -0700

    hot

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isuper - checks for uppercase character
+ * @c: the character is to be checked
+ * Return: 1 if c is uppercase, 0 othrewise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
